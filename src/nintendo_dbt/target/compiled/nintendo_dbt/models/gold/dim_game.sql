e260e49b639464d65e1a64ce43081427b0709c5a
deniz.riedel <deniz.riedel@gmx.de>
2026-05-29 12:58:14 +0200

    Remove dbt artifacts from tracking

--- src/nintendo_dbt/target/compiled/nintendo_dbt/models/gold/dim_game.sql
+++
@@ -1,4 +0,0 @@
-select distinct
-    md5(game_title)  as game_id,
-    game_title       as title
-from "nintendo_sales"."silver"."stg_million_sellers"
--- src/nintendo_dbt/target/compiled/nintendo_dbt/models/gold/dim_platform.sql
+++
@@ -1,4 +0,0 @@
-select distinct
-    md5(platform_name)  as platform_id,
-    platform_name       as name
-from "nintendo_sales"."silver"."stg_million_sellers"
